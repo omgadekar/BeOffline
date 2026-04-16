733eb82d9ad1cd025ce4d522df4293c91f0a139b
OM <gadekarom@gmail.com>
2026-04-16 22:47:28 +0530

    Solving teams notification issue

--- app/build/generated/hilt/component_sources/debug/com/beoffline/app/DaggerBeOfflineApp_HiltComponents_SingletonC.java
+++ app/build/generated/hilt/component_sources/debug/com/beoffline/app/DaggerBeOfflineApp_HiltComponents_SingletonC.java
@@ -436,18 +436,18 @@ public final class DaggerBeOfflineApp_HiltComponents_SingletonC {
     private static final class LazyClassKeyProvider {
       static String com_beoffline_app_ui_screens_RuleCreatorViewModel = "com.beoffline.app.ui.screens.RuleCreatorViewModel";
 
-      static String com_beoffline_app_ui_screens_AppPickerViewModel = "com.beoffline.app.ui.screens.AppPickerViewModel";
-
       static String com_beoffline_app_ui_screens_DashboardViewModel = "com.beoffline.app.ui.screens.DashboardViewModel";
+
+      static String com_beoffline_app_ui_screens_AppPickerViewModel = "com.beoffline.app.ui.screens.AppPickerViewModel";
 
       @KeepFieldType
       RuleCreatorViewModel com_beoffline_app_ui_screens_RuleCreatorViewModel2;
 
       @KeepFieldType
-      AppPickerViewModel com_beoffline_app_ui_screens_AppPickerViewModel2;
+      DashboardViewModel com_beoffline_app_ui_screens_DashboardViewModel2;
 
       @KeepFieldType
-      DashboardViewModel com_beoffline_app_ui_screens_DashboardViewModel2;
+      AppPickerViewModel com_beoffline_app_ui_screens_AppPickerViewModel2;
     }
   }
 
@@ -496,17 +496,17 @@ public final class DaggerBeOfflineApp_HiltComponents_SingletonC {
 
     @IdentifierNameString
     private static final class LazyClassKeyProvider {
-      static String com_beoffline_app_ui_screens_DashboardViewModel = "com.beoffline.app.ui.screens.DashboardViewModel";
-
       static String com_beoffline_app_ui_screens_RuleCreatorViewModel = "com.beoffline.app.ui.screens.RuleCreatorViewModel";
+
+      static String com_beoffline_app_ui_screens_DashboardViewModel = "com.beoffline.app.ui.screens.DashboardViewModel";
 
       static String com_beoffline_app_ui_screens_AppPickerViewModel = "com.beoffline.app.ui.screens.AppPickerViewModel";
 
       @KeepFieldType
-      DashboardViewModel com_beoffline_app_ui_screens_DashboardViewModel2;
+      RuleCreatorViewModel com_beoffline_app_ui_screens_RuleCreatorViewModel2;
 
       @KeepFieldType
-      RuleCreatorViewModel com_beoffline_app_ui_screens_RuleCreatorViewModel2;
+      DashboardViewModel com_beoffline_app_ui_screens_DashboardViewModel2;
 
       @KeepFieldType
       AppPickerViewModel com_beoffline_app_ui_screens_AppPickerViewModel2;
